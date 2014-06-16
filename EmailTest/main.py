a9dd86f3c01a180174da2711108e18eb57c5749e
Sam Haycraft <phreakdawg@gmail.com>
2014-06-16 15:54:51 -0600

    passing arguments for emails, taking message text as raw input

--- EmailTest/main.py
+++ EmailTest/main.py
@@ -1,14 +1,23 @@
+import sys
+from sys import argv
 import smtplib
 from email.mime.text import MIMEText
 
-msg = MIMEText("You suck")
+if len(argv) != 3:
+    sys.stderr.write('Usage: ' + sys.argv[0] + ' <from_addr> <to_addr>\n')
+    sys.exit(1)
 
-msg['Subject'] = "Python email test"
-msg['From'] = "shaycraft@tcolandservices.com"
-msg['To'] = "phreakdawg@gmail.com"
+from_addr = sys.argv[1]
+to_addr = sys.argv[2]
+
+msg = MIMEText(raw_input())
+
+msg['Subject'] = "Mineral Lease Evaluation Form Web Submission"
+msg['From'] = str(from_addr)
+msg['To'] = str(to_addr)
 
 s = smtplib.SMTP('linuxserver1.tcolandservices.com')
 
-s.sendmail('shaycraft@tcolandservices.com','phreakdawg@gmail.com',msg.as_string())
+s.sendmail('shaycraft@tcolandservices.com', 'phreakdawg@gmail.com', msg.as_string())
 
-print msg.as_string()
+print "Email sent"
