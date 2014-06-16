22d85d09105a697167d3e86c10f0939fe8cc0732
shaycraft <pnutmeow2600)>
2014-06-16 16:55:40 -0600

    stupid fucking unix cannot pass command arguments to the /etc/aliases shit

--- EmailTest/main.py
+++ EmailTest/main.py
@@ -1,22 +1,30 @@
+#!/usr/bin/python
 import sys
 from sys import argv
 import smtplib
 from email.mime.text import MIMEText
 
-if len(argv) != 3:
-    sys.stderr.write('Usage: ' + sys.argv[0] + ' <from_addr> <to_addr>\n')
-    sys.exit(1)
+#if len(argv) != 3:
+#    sys.stderr.write('Usage: ' + sys.argv[0] + ' <from_addr> <to_addr>\n')
+#   sys.exit(1)
 
-from_addr = sys.argv[1]
-to_addr = sys.argv[2]
+from_addr = "minerals@tcolandservices.com"
+to_addr = "phreakdawg@gmail.com"
 
-msg = MIMEText(raw_input())
+msg = ""
+for line in sys.stdin:
+    msg = msg + line
+msg = MIMEText(msg)
+
+#msg =  MIMEText(sys.stdin.readlines())
 
 msg['Subject'] = "Mineral Lease Evaluation Form Web Submission"
 msg['From'] = str(from_addr)
 msg['To'] = str(to_addr)
 
 s = smtplib.SMTP('linuxserver1.tcolandservices.com')
+
+print msg.as_string()
 
 s.sendmail('shaycraft@tcolandservices.com', 'phreakdawg@gmail.com', msg.as_string())
 
