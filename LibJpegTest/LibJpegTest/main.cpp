9f7bbd24f0933334ffdc31bad517a285ae51b163
Sam <phreakdawg@gmail.com>
2014-06-28 15:07:28 -0600

    initial commit libjpegtest

---
+++ LibJpegTest/LibJpegTest/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<stdlib.h>
+#include<jpeglib.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "This is a test.\n";
+	vector<int> v;
+
+	return EXIT_SUCCESS;
+}
